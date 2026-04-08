164274c510e545d09a2e6b6923f212ffcb006678
Daniel Lansana <d4niel.lansana@gmail.com>
2026-04-08 17:14:22 -0300

    Fix media calculation and parse inputs as numbers

--- Atividade3/JavasScript.js
+++ Atividade3/JavasScript.js
@@ -1,19 +1,19 @@
 
 function media(){
     nome = prompt("Seu nome por gentileza");
-    nota1 = prompt("Sua primeira nota");
-    nota2 = prompt("Sua segunda nota");
-    nota3 = prompt("Sua terceira nota");
+    nota1 = parseFloat(prompt("Sua primeira nota"));
+    nota2 = parseFloat(prompt("Sua segunda nota"));
+    nota3 = parseFloat(prompt("Sua terceira nota"));
 
-    media = (nota1 + nota2 + nota3) *3;
+    media = (nota1 + nota2 + nota3) / 3;
 
     alert(nome + " sua média é " + media);
     confirm("Está feliz com isso? Bom, de qualquer jeito essa é sua média ok?");
 }
 
 function calculos(){
-    let num1 = prompt("Digite seu primeiro nùmero");
-    let num2 = prompt("Digite seu segundo número");
+    let num1 = parseFloat(prompt("Digite seu primeiro nùmero"));
+    let num2 = parseFloat(prompt("Digite seu segundo número"));
     let soma = num1 + num2;
 
     alert("Agora olha isso!!!!");
