124606951e2bb33bdb6d485c530cf30e4c3882b0
d4n-oi <d4niel.lansana@gmail.com>
2026-09-02 14:41:12 -0300

    Atividade19

--- Atividade19/App.js
+++ Atividade19/App.js
@@ -1,21 +1,14 @@
 let app = require('./app/config/server');
-
-app.get('/', function(req, res){
-    res.render("home/index.ejs");
-    });
-app.get('/formulario_adicionar_usuario', function(req,res){
-    res.render("admin/adicionar_usuario");
-});
-app.get('/informacoes/historia', function(req, res){
-    res.render('informacoes/historia');
-    });
-
-app.get('/informacoes/cursos', function(req, res){
-    res.render('informacoes/cursos');
-    });
-app.get('/informacoes/professores', function(req, res){
-    res.render('informacoes/professores');
-    });
+let rotaHome =require('./app/routes/home.js');
+rotaHome(app);
+let rotaAddUsuario = require('./app/routes/adicionar_usuario.js');
+rotaAddUsuario(app);
+let rotaHistoria = require('./app/routes/historia.js');
+rotaHistoria(app);
+let rotaCursos = require('./app/routes/cursos.js');
+rotaCursos(app);
+let rotaProfs = require('./app/routes/professores.js')
+rotaProfs(app);
 app.listen(3000, function(){
     console.log("Servidou iniciado")
 });
